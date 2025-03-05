fc07d56610f20745c5348ff49cb8da139953ef23
roxep5 <58743609+roxep5@users.noreply.github.com>
2025-03-05 10:19:18 +0100

    changeNOptionsToMatSelect

--- psychouned/src/app/shared/gradeCalculator/gradeCalculator.component.ts
+++ psychouned/src/app/shared/gradeCalculator/gradeCalculator.component.ts
@@ -1,16 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroupDirective, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { TestsPageRoutingModule } from 'src/app/pages/tests/tests-routing.module';
 import { MatInputModule } from '@angular/material/input';
 import { GlobalsService } from 'src/app/services/Globals.service';
 import { Grade } from 'src/app/models/Grade';
 import { GradeService } from 'src/app/services/Grade.service';
-import { ErrorStateMatcher } from '@angular/material/core';
 import { MatIconModule } from '@angular/material/icon';
 import { InputComponentComponent } from '../input-component/input-component.component';
 import {MatSelectModule} from '@angular/material/select';
+import { SelectComponentComponent } from '../select-component/select-component.component';
 
 @Component({
     selector: 'app-grade-calculator',
@@ -24,17 +23,19 @@ import {MatSelectModule} from '@angular/material/select';
         MatSelectModule,
         MatIconModule,
         ReactiveFormsModule,
-        InputComponentComponent
+        InputComponentComponent,
+        SelectComponentComponent
     ],
 })
 export class GradeCalculatorComponent {
     @Output() gradeOut = new EventEmitter<Grade>();
     resultGrade = 0;
     totalQuestionsForm = new FormControl(0, [Validators.required, Validators.min(10), Validators.max(50)]);
-    totalOptionsForm = new FormControl(2, [Validators.required, Validators.min(2), Validators.max(5)]);
+    totalOptionsForm = new FormControl(2);
     successesForm = new FormControl(0, [Validators.min(0), Validators.max(99)]);
     mistakesForm = new FormControl(0, [Validators.min(0), Validators.max(99)]);
     answersBlankForm = new FormControl(0, [Validators.min(0), Validators.max(99)]);
+    options=[2,3,4,5]
     get totalOptions() {
         return Number(this.totalOptionsForm.value);
     }
@@ -55,28 +56,12 @@ export class GradeCalculatorComponent {
     ) { }
 
     submit() {
-        console.log('preguntas totales: ', this.totalQuestions);
-        console.log('opciones totales: ', this.totalOptions);
-        console.log('Aciertos: ', this.successes);
-        console.log('Errores: ', this.mistakes);
-        console.log('En blanco: ', this.answersBlank);
-
-        if (this.validateOptions(this.totalOptions) && this.totalQuestions) {
+        if (this.totalQuestions) {
             this.changeValuesWith0();
             this.calculateGrade();
         } else if (this.validateQuestionsAndOptions(this.totalOptions, this.totalQuestions)) {
             this.globalsService.showToast('Se necesita un total de preguntas y de opciones');
-        } else if (!this.validateOptions(this.totalOptions)) {
-            this.globalsService.showToast('El total de opciones debe ser entre 2 y 5');
         }
-    }
-
-    validateOptions(totalOptions: any): boolean {
-        if (totalOptions >= 2 && totalOptions <= 5) {
-            return true;
-        }
-
-        return false;
     }
     validateQuestionsAndOptions(totalOptions: number, totalQuestions: number): boolean {
         if (totalOptions !== 0 && totalQuestions !== 0) {
@@ -101,7 +86,6 @@ export class GradeCalculatorComponent {
     }
     changeQuestion(event: any) {
         this.totalQuestionsForm.setValue(event);
-        console.log(this.totalQuestions)
     }
     changeMistakes(event: any) {
         this.mistakesForm.setValue(event);
